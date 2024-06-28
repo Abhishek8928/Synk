5acefdedd192f169d69b7f7a33f5442ec45443af
= <abhishekshaxma8356@gmail.com>
2024-06-29 00:33:38 +0530

    v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     window.addEventListener('scroll', function() {
       const navbar = document.querySelector('header');
       if (window.scrollY > 0) {
-          navbar.style.backgroundColor = 'rgb(10,10,11,0.8)'; 
+          navbar.style.backgroundColor = 'rgba(11, 11, 11, 0.32)'; 
       } else {
           navbar.style.backgroundColor = 'rgb(10, 10, 11)'; 
       }
